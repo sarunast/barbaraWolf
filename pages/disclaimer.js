6da1c4e56a40d500788b7935538364747804d9e2
Sarunas Tamasauskas <sarunas.tamasauskas@futurice.com>
2017-10-31 12:57:02 +0100

    Add Disclaimer page

--- pages/disclaimer.js
+++ pages/disclaimer.js
@@ -1,23 +1,28 @@
 import React from 'react'
 import PropsTypes from 'prop-types'
+import { RichText } from 'prismic-reactjs'
+import hump from 'lodash-humps'
+
+import Api from '../services/prismic'
 import Layout from '../components/Layout'
 
-const Disclaimer = ({ metaTitle, metaDescription }) => (
+const Disclaimer = ({ metaTitle, metaDescription, text }) => (
   <Layout metaTitle={metaTitle} metaDescription={metaDescription}>
-    <span>Hello world !</span>
+    {RichText.render(text)}
   </Layout>
 )
 
 Disclaimer.propTypes = {
   metaTitle: PropsTypes.string.isRequired,
   metaDescription: PropsTypes.string.isRequired,
+  // eslint-disable-next-line react/forbid-prop-types
+  text: PropsTypes.any.isRequired,
 }
 
 Disclaimer.getInitialProps = async function getInitialProps() {
-  return {
-    metaTitle: 'test',
-    metaDescription: 'description',
-  }
+  const disclaimerPageData = await Api.getDisclaimerPage()
+
+  return hump(disclaimerPageData)
 }
 
 export default Disclaimer
